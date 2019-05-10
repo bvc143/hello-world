ed44d11f25b17fb0d6256f536918373965b26e57
bvc143 <50482721+bvc143@users.noreply.github.com>
2019-05-10 15:07:16 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution{
+    public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ur name:");
+		String Name = sc.nextLine();
+		System.out.println("Hello dear"+Name+"   Thanks for visiting my file   ");
+		}
+	}
+		      
